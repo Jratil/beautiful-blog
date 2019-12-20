296b6dc3f66876deeb78d71e61ebf5aa7de5cce5
lxxxv5 <1490769790@qq.com>
2019-12-20 14:48:55 +0800

    chore: add typings.d.ts

---
+++ fd/typings/main.d.ts
@@ -0,0 +1,8 @@
+declare module '*.css'
+declare module '*.less'
+declare module '*.png'
+declare module '*.svg'
+declare module '*.gif'
+declare module '*.ts'
+declare module '*.pdf'
+declare module '*.zip'
