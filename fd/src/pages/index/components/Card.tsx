71f1ec938af60f2ad387f4f85b9af52452b96cf8
lxxxv5 <1490769790@qq.com>
2020-02-05 15:27:55 +0800

    feat(home): time format

--- fd/src/pages/index/components/Card.tsx
+++ fd/src/pages/index/components/Card.tsx
@@ -3,6 +3,7 @@ import styles from '../index.less'
 import { Icon } from 'antd'
 import { Link } from 'umi'
 import { IArticle } from '../model'
+import moment from 'moment'
 
 interface IProps {
     data: IArticle
@@ -16,16 +17,16 @@ const Card: React.FC<IProps> = ({ data }) => {
             <div className={styles.card_subtitle}>{articleSubtitle}</div>
             <div className={styles.card_footer}>
                 <span className={styles.fl}>
-                    <Icon type='user' className={styles.card_icon} />
+                    <Icon type="user" className={styles.card_icon} />
                     {authorName}
                 </span>
                 <span className={styles.fl}>
-                    <Icon type='like' className={styles.card_icon} />
+                    <Icon type="like" className={styles.card_icon} />
                     {articleLike}
                 </span>
                 <span className={styles.fr}>
-                    <Icon type='history' className={styles.card_icon} />
-                    {lastUpdate}
+                    <Icon type="history" className={styles.card_icon} />
+                    {moment(Number(lastUpdate) * 1000).format('YYYY-MM-DD HH:mm:ss')}
                 </span>
             </div>
         </Link>
